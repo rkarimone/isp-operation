8fd063ebc88923ea0ff612e1643d5885aa5bf340
REZAUL KARIM <110754378+rkarimone@users.noreply.github.com>
2023-05-13 14:40:47 +0600

    Create 004-nfsen-nfdump-u20.04.sh

---
+++ 004-nfsen-nfdump-u20.04.sh
@@ -0,0 +1,5 @@
+https://ipcorenetworks.blogspot.com/2021/08/installing-nfsen-nfdump-on-ubuntu-and.html
+https://ws.learn.ac.lk/wiki/NspwUprouse/Agenda/netflow
+https://ipcorenetworks.blogspot.com/2022/02/installing-nfsen-nfdump-porttracker.html
+https://ipcorenetworks.blogspot.com/2021/09/how-to-install-configure-loganalyzer.html
+-- https://www.youtube.com/watch?v=wlLvJcFqHYw
